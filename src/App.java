ea23884252d14893b0a536d6160167038bfc51ac
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2023-06-19 08:35:31 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import gui.*;
+import gui.botoes.*;
+import videos.*;
+
+public class App{
+    public static void main(String[] args) throws Exception {
+        Filmes filme = new Filmes();
+        Filmes filme2 = new Filmes();
+
+
+        Novela novela = new Novela();
+
+        Documentario documentario = new Documentario();
+
+        Serie serie = new Serie();
+        
+        Anime anime = new Anime();
+
+        Janela janelinha = new Janela();
+        MiniaturaVideo miniatura = new MiniaturaVideo();
+
+
+
+
+
+    }
+}
+
